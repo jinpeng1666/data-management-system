b1e0377c9768427640c4b2d8df5d064a55ba1de3
JP.Zhou <3214423826@qq.com>
2024-05-15 20:37:26 +0800

    test: 测试prettier是否配置成功

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 
-createApp(App).mount("#app");
-
-let a = 100;
+createApp(App).mount('#app')
