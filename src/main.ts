4863370d0c15fc12e90724bca99849da5bd938a8
JP.Zhou <3214423826@qq.com>
2024-05-16 12:37:37 +0800

    test: 测试element-plus

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// 按需引入element相关的组件和样式，并使用
+import { ElButton } from 'element-plus'
+import 'element-plus/dist/index.css'
+app.use(ElButton)
+
+app.mount('#app')
