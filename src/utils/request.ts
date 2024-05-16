5301e0cd7f27a5bc7c17f8076e3b67551d7e7b62
JP.Zhou <3214423826@qq.com>
2024-05-16 21:50:08 +0800

    chore: 对axios进行二次封装

---
+++ src/utils/request.ts
@@ -0,0 +1,35 @@
+// 对axios进行二次封装
+
+import axios from 'axios'
+
+// 引入element-plus的组件
+import { ElMessage } from 'element-plus'
+
+const request = axios.create({
+  baseURL: '/api',
+  timeout: 5000,
+})
+
+// 请求拦截器
+request.interceptors.request.use((config) => {
+  return config
+})
+
+// 响应拦截器
+request.interceptors.response.use(
+  // 响应成功回调
+  (response) => {
+    // 用于处理（简化数据）请求返回的数据
+    return response.data
+  },
+  // 响应失败回调
+  (error) => {
+    ElMessage({
+      type: 'error',
+      message: '请求错误',
+    })
+    return Promise.reject(error)
+  },
+)
+
+export default request
