2493f2309e9ee63fc33bc31eb8085492bddc7fcb
JP.Zhou <3214423826@qq.com>
2024-05-20 12:31:16 +0800

    feat: 新增登录api和获取用户信息api

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,14 +1,42 @@
 import { defineStore } from 'pinia'
 
+// 导入登录请求的api
+import { reqLogin, reqUserInfo } from '@/api/user'
+
+// 导入js-cookie
+import { getToken, setToken, removeToken } from '@/utils/auth'
+
 const useUserStore = defineStore('User', {
   state: () => {
     return {
-      token: '',
+      token: getToken() as string,
       userName: '',
       avatar: '',
     }
   },
-  actions: {},
+  actions: {
+    // 登录api
+    async userLogin(data: any) {
+      // 调用登录请求的api
+      const result: any = await reqLogin(data)
+      // 请求成功则将放回的token存储在本地
+      if (result.code == 200) {
+        // 设置仓库中token
+        this.token = result.data.token as string
+        // 存储在cookie
+        setToken(result.data.token)
+        return 'ok'
+      } else {
+        return Promise.reject(new Error(result.data.message))
+      }
+    },
+    // 获取userName和avatar(该函数使用的前提是token存在)
+    async userMessage() {
+      const result = await reqUserInfo()
+      this.avatar = result.data.checkUser.avatar
+      this.userName = result.data.checkUser.username
+    },
+  },
   getters: {},
 })
 
