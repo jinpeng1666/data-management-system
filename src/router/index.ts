582c75e2a22024e283634e30de8db7c57353d6c1
JP.Zhou <3214423826@qq.com>
2024-05-20 12:30:28 +0800

    feat: 全局前置路由初始化

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,12 @@ import { constantRouterMap } from './routes'
 import nprogress from 'nprogress'
 import 'nprogress/nprogress.css'
 
+// 免登录白名单
+const whiteList = ['/login', '/404']
+
+// 引入user仓库
+import useUserStore from '@/store/modules/user'
+
 // 创建路由
 const router = createRouter({
   history: createWebHashHistory(),
@@ -19,8 +25,32 @@ const router = createRouter({
 
 // 全局前置路由
 router.beforeEach((to, from, next) => {
+  const userStore = useUserStore()
   nprogress.start()
-  next()
+  if (userStore.token) {
+    // 第一层判断：token值存在
+    if (userStore.avatar === '' && userStore.userName === '') {
+      userStore.userMessage()
+    }
+    if (to.path === '/login') {
+      // 第二层判断：路由路径是登陆页面路径
+      next('/')
+      nprogress.done()
+    } else {
+      // 第二层判断：路由路径不是登陆页面路径
+      next()
+    }
+  } else {
+    // 第一层判断：token值不存在
+    if (whiteList.includes(to.path)) {
+      // 第二层判断：路由路径在免登录白名单上
+      next()
+    } else {
+      // 第二层登录：登录路径不在免登录白名单上
+      next('/login')
+      nprogress.done()
+    }
+  }
 })
 
 // 全局后置路由
