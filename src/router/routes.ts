c45189b6806326067bbb80aea2895cf9e2b79d64
JP.Zhou <3214423826@qq.com>
2024-05-20 23:26:24 +0800

    feat: 完善route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -16,9 +16,51 @@ export const constantRouterMap = [
         path: '/home',
         component: () => import('@/views/home/index.vue'),
       },
+      {
+        path: '/salary',
+        component: () => import('@/views/salary/index.vue'),
+      },
+      {
+        path: '/selfAttendance',
+        component: () => import('@/views/selfAttendance/index.vue'),
+      },
     ],
   },
 ]
 
 // 存放需要根据权限动态加载的路由表
-export const asyncRouterMap = []
+export const asyncRouterMap = [
+  {
+    path: '/',
+    component: () => import('@/layout/index.vue'),
+    name: 'layout',
+    redirect: '/home',
+    children: [
+      {
+        path: '/employee',
+        component: () => import('@/views/employee/index.vue'),
+        meta: { role: ['Manager', 'Minister'] },
+      },
+      {
+        path: '/finance',
+        component: () => import('@/views/finance/index.vue'),
+        meta: { role: ['Manager', 'Finance'] },
+      },
+      {
+        path: '/attendance',
+        component: () => import('@/views/attendance/index.vue'),
+        meta: { role: ['Manager', 'Attendance'] },
+      },
+      {
+        path: '/operation',
+        component: () => import('@/views/operation/index.vue'),
+        meta: { role: ['Manager', 'Operation'] },
+      },
+      {
+        path: '/authority',
+        component: () => import('@/views/authority/index.vue'),
+        meta: { role: ['Manager'] },
+      },
+    ],
+  },
+]
