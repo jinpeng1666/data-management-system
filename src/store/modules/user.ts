2ab8dafa0fc911822a75c093f5977d70106e6717
JP.Zhou <3214423826@qq.com>
2024-05-21 21:56:21 +0800

    feat: 完成动态路由表的过滤

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -6,12 +6,24 @@ import { reqLogin, reqUserInfo } from '@/api/user'
 // 导入js-cookie
 import { getToken, setToken, removeToken } from '@/utils/auth'
 
+// 导入route
+import { constantRouterMap, asyncRouterMap } from '@/router/routes'
+
+// 引入permission方法
+import { hasPermission } from '@/utils/permission.ts'
+
+import { useRouter } from 'vue-router'
+
 const useUserStore = defineStore('User', {
   state: () => {
     return {
       token: getToken() as string,
-      userName: '',
-      avatar: '',
+      info: {
+        userName: '',
+        avatar: '',
+        roles: [],
+      },
+      routes: [],
     }
   },
   actions: {
@@ -30,11 +42,35 @@ const useUserStore = defineStore('User', {
         return Promise.reject(new Error(result.data.message))
       }
     },
-    // 获取userName和avatar(该函数使用的前提是token存在)
+    // 获取userName、avatar和roels，同时计算出routes(该函数使用的前提是token存在)
     async userMessage() {
       const result = await reqUserInfo()
-      this.avatar = result.data.checkUser.avatar
-      this.userName = result.data.checkUser.username
+      this.info.avatar = result.data.checkUser.avatar
+      this.info.userName = result.data.checkUser.username
+      this.info.roles = result.data.checkUser.roles
+      // 过滤动态路由表，路由是二级路由，还需和一级路由合并
+      const filterAsyncRouterMap = asyncRouterMap[0].children.filter(
+        (route) => {
+          if (hasPermission(this.info.roles, route)) {
+            return true
+          }
+          return false
+        },
+      )
+      const addAsyncRouterMap = [
+        {
+          path: '/',
+          component: () => import('@/layout/index.vue'),
+          name: 'layout',
+          redirect: '/home',
+          children: filterAsyncRouterMap,
+        },
+      ]
+
+      console.log(addAsyncRouterMap)
+      // 获取路由器
+      // const $router = useRouter()
+      // $router.addRoute(addAsyncRouterMap)
     },
   },
   getters: {},
