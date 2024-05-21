3f3698e386c4071a09ed5a54648be5ab9473c7e1
JP.Zhou <3214423826@qq.com>
2024-05-21 21:55:15 +0800

    fix: 修复无法校验权限的问题

--- src/utils/permission.ts
+++ src/utils/permission.ts
@@ -1,10 +1,8 @@
 // 校验用户是否具有访问这个路由的权限
 // roles：用户身份
-// route：路由
-export function hasPermission(roles: any, route: any) {
-  if (route.meta && route.meta.role) {
-    return roles.some((role: any) => route.meta.role.indexOf(role) >= 0)
-  } else {
-    return true
-  }
+// routes：传入动态路由表中二级路由对象
+export function hasPermission(roles: any, routes: any) {
+  return roles.some((role: any) => {
+    return routes.meta.role.includes(role)
+  })
 }
