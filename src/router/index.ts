fa540617e90dbef7edcb0b96fd1edef8761d108a
JP.Zhou <3214423826@qq.com>
2024-05-21 09:24:27 +0800

    feat: 全局前置路由守卫基本搭建完毕

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,6 +11,9 @@ const whiteList = ['/login', '/404']
 // 引入user仓库
 import useUserStore from '@/store/modules/user'
 
+// 引入permission方法
+import { hasPermission } from '@/utils/permission.ts'
+
 // 创建路由
 const router = createRouter({
   history: createWebHashHistory(),
@@ -24,21 +27,52 @@ const router = createRouter({
 })
 
 // 全局前置路由
-router.beforeEach((to, from, next) => {
+router.beforeEach(async (to, from, next) => {
   const userStore = useUserStore()
   nprogress.start()
   if (userStore.token) {
     // 第一层判断：token值存在
-    if (userStore.avatar === '' && userStore.userName === '') {
-      userStore.userMessage()
-    }
     if (to.path === '/login') {
       // 第二层判断：路由路径是登陆页面路径
       next('/')
       nprogress.done()
     } else {
       // 第二层判断：路由路径不是登陆页面路径
-      next()
+      if (to.meta && to.meta.role) {
+        // 第三层判断：页面需要权限
+        if (userStore.info.avatar === '' || userStore.info.userName === '') {
+          // 第四层判断：用户数据已经获取
+          if (hasPermission(userStore.info.roles, to)) {
+            // 第五层判断：用户具有权限
+            next()
+          } else {
+            // 第五层判断：用户不具有权限
+            next('/404')
+          }
+        } else {
+          // 第四层判断：用户数据未获取
+          // 获取用户信息，然后跳转
+          await userStore.userMessage()
+          if (hasPermission(userStore.info.roles, to)) {
+            // 第五层判断：用户具有权限
+            next()
+          } else {
+            // 第五层判断：用户不具有权限
+            next('/404')
+          }
+        }
+      } else {
+        // 第三层判断：页面不需要权限
+        if (userStore.info.avatar === '' || userStore.info.userName === '') {
+          // 第四层判断：用户数据已经获取
+          next()
+        } else {
+          // 第四层判断：用户数据未获取
+          // 获取用户信息，然后跳转
+          await userStore.userMessage()
+          next()
+        }
+      }
     }
   } else {
     // 第一层判断：token值不存在
