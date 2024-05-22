4f99b194c9032a7c330f08a2f6e9f90cc8b8f6f5
JP.Zhou <3214423826@qq.com>
2024-05-22 20:07:53 +0800

    feat: 使用addRoute方法将过滤后动态路由表添加到路由器上

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,18 @@
 <template>我是home组件</template>
 
-<script>
-export default {}
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import useUserStore from '@/store/modules/user'
+const userStore = useUserStore()
+
+// 添加动态路由
+onMounted(() => {
+  const $router = useRouter()
+  userStore.filterAsyncRouterMap.forEach((route: any) => {
+    $router.addRoute('layout', route)
+  })
+})
 </script>
 
 <style></style>
