14b45051b01f51ab28d258c38242f967b4f94225
JP.Zhou <3214423826@qq.com>
2024-05-22 20:08:27 +0800

    test: 添加基础模板，用于测试

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,20 @@
-<template>我是登录组件</template>
+<template>
+  我是登录组件
+  <el-button @click="login">登录</el-button>
+</template>
 
-<script>
-export default {}
+<script setup lang="ts">
+// 引入仓库
+import useUserStore from '@/store/modules/user'
+let userStore = useUserStore()
+
+import { useRouter } from 'vue-router'
+// 获取路由器
+let $router = useRouter()
+const login = () => {
+  userStore.userLogin({ username: 'AttendanceMinister', password: '222222' })
+  $router.push('/')
+}
 </script>
 
 <style></style>
