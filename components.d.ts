ebc58b02b6f2237e2dc0d8d59cd0cfaedc673b77
JP.Zhou <3214423826@qq.com>
2024-05-26 20:13:28 +0800

    chore: 配置ElCheckbox

--- components.d.ts
+++ components.d.ts
@@ -8,6 +8,7 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElCol: typeof import('element-plus/es')['ElCol']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
