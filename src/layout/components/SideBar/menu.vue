ed60f7fa465b46f3c18b6044cf17b2622d5566bd
JP.Zhou <3214423826@qq.com>
2024-05-27 20:42:36 +0800

    feat: mene组件的初始化

--- src/layout/components/SideBar/menu.vue
+++ src/layout/components/SideBar/menu.vue
@@ -1,7 +1,42 @@
 <template>
-  <div>我是menu组件</div>
+  <el-scrollbar class="menuScrollbar">
+    <!-- element菜单 -->
+    <el-menu
+      router
+      style="background-color: #a3b18a; width: 100%"
+      :default-active="$route.path"
+    >
+      <template v-for="item in menuList" :key="item.path">
+        <!-- 没有子路由 -->
+        <el-menu-item v-if="item.children.length <= 1" :index="item.path">
+          <template #title>{{ item.meta.title }}</template>
+        </el-menu-item>
+        <!-- 有子路由 -->
+        <el-sub-menu v-if="item.children.length > 1" :index="item.path">
+          <template #title>{{ item.meta.title }}</template>
+          <Menu :menuList="item.children"></Menu>
+        </el-sub-menu>
+      </template>
+    </el-menu>
+  </el-scrollbar>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// console.log(1)
+// 获取路由器
+import { useRouter } from 'vue-router'
+let $router = useRouter()
 
-<style scoped lang="scss"></style>
+// 需要在左侧导航栏展示的路由
+let menuList = $router.getRoutes().filter((route) => {
+  return !route.meta.isHidden
+})
+console.log(menuList)
+</script>
+
+<style scoped lang="scss">
+.menuScrollbar {
+  width: 100%;
+  height: calc(100vh - $logo-height);
+}
+</style>
