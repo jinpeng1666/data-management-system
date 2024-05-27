2b83d8938848442d50b07a0b23f5b4828eee871d
JP.Zhou <3214423826@qq.com>
2024-05-27 19:50:19 +0800

    feat: 修改动态添加路由的时机，并添加404路由

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -24,14 +24,34 @@
 <script setup lang="ts">
 import SideBar from './components/SideBar/index.vue'
 import HeadBar from './components/HeadBar/index.vue'
+
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import useUserStore from '@/store/modules/user'
+const userStore = useUserStore()
+// 添加动态路由
+onMounted(() => {
+  const $router = useRouter()
+  userStore.filterAsyncRouterMap.forEach((route: any) => {
+    $router.addRoute('layout', route)
+  })
+  // 最后加上404路由
+  $router.addRoute({
+    path: '/:pathMatch(.*)*',
+    component: () => import('@/views/404/index.vue'),
+    name: '404',
+  })
+})
 </script>
 
 <style scoped lang="scss">
+@import '@/styles/variable.scss';
+
 .common-layout {
   height: 100vh;
   background-color: pink;
   .aside {
-    width: 200px;
+    width: $el-aside-width;
     background-color: bisque;
   }
 }
