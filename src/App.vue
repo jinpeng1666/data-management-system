92d0ba030fc51ada6025bad0087040ac726c722d
JP.Zhou <3214423826@qq.com>
2024-05-28 19:09:08 +0800

    fix: 修复刷新动态路由消失的问题

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
-<script setup lang="ts"></script>
-
 <template>
   <div>
     <router-view></router-view>
   </div>
 </template>
+
+<script setup lang="ts">
+// 解决页面刷新，动态路由消失的问题
+import { useRouter } from 'vue-router'
+const $router = useRouter()
+import useUserStore from './store/modules/user'
+const userStore = useUserStore()
+if (userStore.info.avatar === '' || userStore.info.userName === '') {
+  $router.push('/')
+}
+</script>
 
 <style scoped></style>
